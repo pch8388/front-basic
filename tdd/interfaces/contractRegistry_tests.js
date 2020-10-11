be8fd88103104e2211e4eadb9a475f9341e88032
pch8388 <pch838811@gmail.com>
2020-10-12 00:44:00 +0900

    [feat] javascript 패턴과 테스트 : interface 학습 - 1

---
+++ tdd/interfaces/contractRegistry_tests.js
@@ -0,0 +1,54 @@
+describe('contractRegistry', () => {
+    'use strict';
+    let registry;
+    let isArray = 'isArray';
+    const ary = [1, 2, 3];
+
+    beforeEach(() => {
+        registry = ReliableJavaScript.contractRegistry();
+        registry.define(isArray, Array.isArray);
+    });
+
+    describe('define(contractName, evaluator)', () => {
+        it('contractName 이 문자열이 아니면 예외를 던진다', () => {
+            expect(() => {
+                registry.define(undefined, () => {
+                });
+            }).toThrow(new Error(registry.messages.nameMustBeString));
+        });
+
+        it('evaluator 가 함수가 아니면 예외를 던진다', () => {
+            expect(() => {
+                registry.define('myContract', '함수 아님')
+            }).toThrow(new Error(registry.messages.evaluatorMustBeFunction));
+        });
+
+        it('contractName 이 문자열이고 evaluator 가 함수면 예외를 던지지 않는다.', () => {
+            expect(() => {
+                registry.define('myContract', () => {
+                });
+            }).not.toThrow();
+        });
+    });
+
+    describe('fulfills(contractName, obj)', () => {
+        it('contractName 이 레지스트리에 없으면 예외를 던진다.', () => {
+            function expectThrow(contractName) {
+                expect(() => {
+                    registry.fulfills(contractName, {});
+                }).toThrow(new Error(
+                    registry.getMessageForNameNotRegistered(contractName)));
+            }
+
+            [undefined, 'abc'].forEach(expectThrow);
+        });
+
+        it('객체가 규약을 지키면 true 반환', () => {
+            expect(registry.fulfills(isArray, ary)).toBe(true);
+        });
+
+        it('객체가 규약을 위반하면 false 반환', () => {
+            expect(registry.fulfills(isArray, 'not an array')).toBe(false);
+        });
+    });
+});
